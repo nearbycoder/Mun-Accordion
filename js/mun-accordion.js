24a5bc5ec811406074c732289aa9490fef7f7ecd
nearbycoder <nearbycoder@gmail.com>
2016-01-29 18:32:54 -0600

    Update repo to reflect changes made in JSBin
    A few changes where made in JSBin such as the h5 css as well as some
    ending semicolons that were originally throwing errors in jsbin.

--- js/mun-accordion.js
+++ js/mun-accordion.js
@@ -1,5 +1,6 @@
+/*jshint multistr: true */
 (function() {
-  'use strict'
+  'use strict';
   angular
   	//instantiate module for accordion.
   	//naming scheme comes from Dave Smiths talk about using company initial for the beginning of your directives
@@ -8,21 +9,21 @@
     //controller created to store simple logic for the directive state
     .controller('munAccordionController', ['$scope', function($scope){
     	//create array to hold accordion Items state
-      var accordionItems = []
+      var accordionItems = [];
       //on each accordion header click check if multiple="true" is set, else set all accordion items to false.
       this.openAccordionItem = function(accordionItemToOpen) {
         if( !$scope.multiple ) {
           accordionItems.forEach(function(accordionItem) {
-            accordionItem.isOpenned = false
-          })
+            accordionItem.isOpenned = false;
+          });
         }
         //set currently clicked AccordionItem to opened.
-				accordionItemToOpen.isOpenned = true       
-      }
+				accordionItemToOpen.isOpenned = true;        
+      };
       //Function ran on intial load to get all accordionItem in accordionItems array.
       this.addAccordionItem = function(accordionItem) {
-        accordionItems.push(accordionItem)
-      }
+        accordionItems.push(accordionItem);
+      };
     }])
     //Directive for outside wrapper. 
     .directive('munAccordion', function() {
@@ -38,7 +39,7 @@
 	      },
 	      controller: 'munAccordionController',
 	      template: '<div class="accordion" ng-transclude></div>'
-	    }
+	    };
   	})
   	//Main directive for each accordion item.
     .directive('accordionItem', function() {
@@ -55,17 +56,17 @@
 	      },
 	      link: function(scope, element, attrs, accordionController) {
 	      	//set isOpened on accordionItem with initiallyOpen true.
-	        scope.isOpenned = (scope.initiallyOpen) ? true : false
+	        scope.isOpenned = (scope.initiallyOpen) ? true : false;
 	        //push the state of each accordionItem to the accordionController
-	        accordionController.addAccordionItem(scope)
+	        accordionController.addAccordionItem(scope);
 	        //simple function to check isOpenned state and apply the openAccordionItem function from the accordionController passing the item clicked.
 	        scope.toggleAccordionItem = function () {
 	          if(!scope.isOpenned) {
-	            accordionController.openAccordionItem(this)
+	            accordionController.openAccordionItem(this);
 	          } else {
-	            scope.isOpenned = false
+	            scope.isOpenned = false;
 	          }
-	        }
+	        };
 	      },
 	      template: '\
 	      <div class="accordion-item" ng-class="{open: isOpenned, closed: !isOpenned}"> \
@@ -77,6 +78,6 @@
 	        	<div class="content" ng-transclude></div> \
 	        </div> \
 	      </div>'
-	    }
-  	})
-})()
+	    };
+  	});
+})();
